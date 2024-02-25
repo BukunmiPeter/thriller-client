50bb4e9b9b797bc70538990bb65eaeefc26feb9c
BukunmiPeter <oluwabukunmiadeyemo99@gmail.com>
2024-02-25 01:43:13 +0100

    ..

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css';
+import  ' ./index.css';
 import { Toaster } from 'react-hot-toast';
 import { Provider } from 'react-redux';
 import store,{persistor } from './redux/store';
